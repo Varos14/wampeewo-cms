5a4e6e32ae7dd6e33673ad91db44d630e8132912
Varos14 <geraldvaros@gmail.com>
2026-07-10 18:33:34 +0300

    Remove test scripts

--- backend/test3.js
+++
@@ -1,7 +0,0 @@
-fetch('http://localhost:4000/api/auth/login', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ email: 'geraldvaros@gmail.com', password: '@AmGerald14' })
-}).then(async r => {
-  console.log(r.status, await r.json());
-}).catch(console.error);
